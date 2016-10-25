15c1a3e3e89e7a8f90a260d4764ebcb8c1ddb4de
jiazhongjin <jiazj@fengyuntec.com>
2016-10-25 10:11:23 +0800

    u

--- src/main/java/DataAn/storm/impl/IDeviceRecordPersitImpl.java
+++ src/main/java/DataAn/storm/impl/IDeviceRecordPersitImpl.java
@@ -13,55 +13,55 @@ import DataAn.storm.BatchContext;
 import DataAn.storm.IDeviceRecord;
 import DataAn.storm.interfece.IDeviceRecordPersit;
 
-public class IDeviceRecordPersitImpl implements IDeviceRecordPersit {
-
-	private IDeviceRecordPersitImpl(){};
-	
-	public static  IDeviceRecordPersitImpl IDPI =null;
-	
-	public static  IDeviceRecordPersitImpl  getInstence(){
-		if(IDPI==null){
-			IDPI= new IDeviceRecordPersitImpl();			
-		}
-		return IDPI;
-	}
-	
-	
-	@Override
-	public void persist(Map context,BatchContext batchContext,IDeviceRecord... deviceRecords) throws Exception {
-		List<Document> tempList = new ArrayList<Document>();
-		String series = "";
-		String star = "";
-		String deviceName = "";
-		for(IDeviceRecord ir :deviceRecords){
-			series = ir.getSeries();
-			star = ir.getStar();
-			deviceName = ir.getName();
-			Document doc = new Document();				
-			doc.append("status", 1);
-			doc.append("year", DateUtil.format(ir.getTime(), "yyyy"));
-			doc.append("year_month", DateUtil.format(ir.getTime(), "yyyy-MM"));
-			doc.append("year_month_day", DateUtil.format(ir.getTime(), "yyyy-MM-dd"));
-			doc.append("series", series);
-			doc.append("star", star);
-			doc.append("deviceName", deviceName);	
-			doc.append("", 1);
-			for(int i=0;i<ir.getProperties().length;i++){			
-				doc.append(ir.getProperties()[i], ir.getPropertyVals()[i]);
-			}
-			tempList.add(doc);		
-		}		
-		MongodbUtil.getInstance().insertMany(InitMongo.getDataBaseNameBySeriesAndStar(series, star), deviceName, tempList);
-				
-	}
-
-
-	@Override
-	public void persist(Map context, BatchContext batchContext,
-			IDeviceRecord... deviceRecords) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-}
+//public class IDeviceRecordPersitImpl implements IDeviceRecordPersit {
+//
+//	private IDeviceRecordPersitImpl(){};
+//	
+//	public static  IDeviceRecordPersitImpl IDPI =null;
+//	
+//	public static  IDeviceRecordPersitImpl  getInstence(){
+//		if(IDPI==null){
+//			IDPI= new IDeviceRecordPersitImpl();			
+//		}
+//		return IDPI;
+//	}
+//	
+//	
+//	@Override
+//	public void persist(Map context,BatchContext batchContext,IDeviceRecord... deviceRecords) throws Exception {
+//		List<Document> tempList = new ArrayList<Document>();
+//		String series = "";
+//		String star = "";
+//		String deviceName = "";
+//		for(IDeviceRecord ir :deviceRecords){
+//			series = ir.getSeries();
+//			star = ir.getStar();
+//			deviceName = ir.getName();
+//			Document doc = new Document();				
+//			doc.append("status", 1);
+//			doc.append("year", DateUtil.format(ir.getTime(), "yyyy"));
+//			doc.append("year_month", DateUtil.format(ir.getTime(), "yyyy-MM"));
+//			doc.append("year_month_day", DateUtil.format(ir.getTime(), "yyyy-MM-dd"));
+//			doc.append("series", series);
+//			doc.append("star", star);
+//			doc.append("deviceName", deviceName);	
+//			doc.append("", 1);
+//			for(int i=0;i<ir.getProperties().length;i++){			
+//				doc.append(ir.getProperties()[i], ir.getPropertyVals()[i]);
+//			}
+//			tempList.add(doc);		
+//		}		
+//		MongodbUtil.getInstance().insertMany(InitMongo.getDataBaseNameBySeriesAndStar(series, star), deviceName, tempList);
+//				
+//	}
+//
+//
+//	@Override
+//	public void persist(Map context, BatchContext batchContext,
+//			IDeviceRecord... deviceRecords) throws Exception {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//
+//}
