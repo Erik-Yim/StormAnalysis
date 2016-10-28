1cd58e8d242b3ad34780d6ecd25aa5844a4e5e1f
root <snailhu@yahoo.com>
2016-10-28 13:54:32 +0800

    [hu]

--- src/main/java/DataAn/storm/denoise/IDenoiseFilterNodeProcessor.java
+++ src/main/java/DataAn/storm/denoise/IDenoiseFilterNodeProcessor.java
@@ -1,8 +1,13 @@
 package DataAn.storm.denoise;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Vector;
 
+import DataAn.storm.BatchContext;
+import DataAn.storm.DefaultDeviceRecord;
 import DataAn.storm.IDeviceRecord;
  
 /**
@@ -18,7 +23,43 @@ public interface IDenoiseFilterNodeProcessor extends Serializable {
 	class IDenoiseFilterNodeProcessorGetter{
 		
 		public static IDenoiseFilterNodeProcessor get(){
-			return null;
+			return new IDenoiseFilterNodeProcessor(){
+
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void cleanup(List<? extends IDeviceRecord> deviceRecords) {
+					String [] vals = null;
+					String [] param = null;
+					Set<String> invalid = new HashSet<>();
+					for(IDeviceRecord idr:deviceRecords){
+						vals = idr.getPropertyVals();
+						param = idr.getProperties();
+						for(int i=0;i<vals.length;i++){							
+							if(vals[i].contains("#")){
+								invalid.add(param[i]);
+							}
+						}
+					}					
+					for(IDeviceRecord idr:deviceRecords){
+						param = idr.getProperties();
+						vals = idr.getPropertyVals();
+						String [] newparam= new String[param.length-invalid.size()];
+						String [] newvals= new String[param.length-invalid.size()];
+						int j = 0;						
+						for(int i=0;i<param.length;i++){							
+							if(!(invalid.contains(param[i]))){
+								newparam[j] = param[i];
+								newvals[j] = vals[i];
+							}
+							j++;
+						}
+						((DefaultDeviceRecord)idr).setProperties(newparam);
+						((DefaultDeviceRecord)idr).setPropertyVals(newvals);
+					}
+				}
+				
+			};
 		}
 		
 	}
