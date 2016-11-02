f29e23b292114cebc7db6d3de960ea7206c9f389
root <snailhu@yahoo.com>
2016-11-02 11:09:11 +0800

    [hu]

---
+++ src/main/java/DataAn/storm/status/StatusInfo.java
@@ -0,0 +1,26 @@
+package DataAn.storm.status;
+
+public class StatusInfo {
+
+	private String version;
+	
+	private String exceptionInfo;
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getExceptionInfo() {
+		return exceptionInfo;
+	}
+
+	public void setExceptionInfo(String exceptionInfo) {
+		this.exceptionInfo = exceptionInfo;
+	}
+	
+	
+}
