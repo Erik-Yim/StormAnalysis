db63b1a116a10b5f9497e1b9f8fe85b4d934ecae
Shenwp <Shenwp@Shenwp-PC>
2016-11-02 16:13:20 +0800

    shenwp

--- src/main/java/DataAn/storm/zookeeper/CommunicationUtils.java
+++ src/main/java/DataAn/storm/zookeeper/CommunicationUtils.java
@@ -5,6 +5,9 @@ import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.zookeeper.CreateMode;
 
@@ -19,42 +22,68 @@ public class CommunicationUtils implements Serializable{
 	
 	private ZookeeperExecutor executor;
 	
+	
+	private static CommunicationUtils communicationUtils;
+	
+	public synchronized static CommunicationUtils get(final ZookeeperExecutor executor){
+		if(communicationUtils==null){
+			communicationUtils=new CommunicationUtils(executor,true);
+		}
+		return communicationUtils;
+	}
+	
 	public CommunicationUtils(final ZookeeperExecutor executor) {
 		this(executor,true);
 	}
 	public CommunicationUtils(final ZookeeperExecutor executor,boolean start) {
 		this.executor = executor;
 		if(start){
-			executor.watchChildrenPath("/flow-tasks", new ZooKeeperClient.NodeChildrenCallback() {
+			
+			Executors.newScheduledThreadPool(1, new ThreadFactory() {
+				
 				@Override
-				public void call(List<Node> nodes) {
-					Collections.sort(nodes, new Comparator<Node>() {
-						@Override
-						public int compare(Node o1, Node o2) {
-							return pathSequence(o1.getPath())-pathSequence(o2.getPath());
-						}
-					});
+				public Thread newThread(Runnable r) {
+					return new Thread(r, "scanning file queue");
+				}
+			})
+			.scheduleAtFixedRate(new Runnable() {
+				
+				@Override
+				public void run() {
 					
-					if(nodes.size()>0){
-						Node node=nodes.get(0);
-						byte[] bytes=node.getData();
-						if(bytes==null){
-							bytes=executor.getPath(node.getPath());
+					try {
+						List<String> paths= executor.backend().getChildren().forPath("/flow-tasks");
+						Collections.sort(paths, new Comparator<String>() {
+							@Override
+							public int compare(String o1, String o2) {
+								return pathSequence(o1)-pathSequence(o2);
+							}
+						});
+						
+						if(paths.size()>0){
+							String path=paths.get(0);
+							byte[] bytes=executor.getPath(path);
+							Communication communication= JJSON.get().parse(
+									new String(bytes,Charset.forName("utf-8")), Communication.class);
+							communication.setZkPath(path);
+							if(NodeStatus.READY.equals(communication.getStatus())){
+								executor.setPath("/locks/worker-schedule/workflow-trigger/default",
+										JJSON.get().formatObject(communication));
+							}
 						}
-						Communication communication= JJSON.get().parse(
-								new String(bytes,Charset.forName("utf-8")), Communication.class);
-						communication.setZkPath(node.getPath());
-						if(NodeStatus.READY.equals(communication.getStatus())){
-							executor.setPath("/locks/worker-schedule/workflow-trigger/default",
-									JJSON.get().formatObject(communication));
-						}
+						
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
 				}
+				
 				private int pathSequence(String path){
 					String lastStr=path.substring(path.lastIndexOf("/"));
 					return Integer.parseInt(lastStr.substring(lastStr.lastIndexOf("-")+1));
 				}
-			});
+				
+			}, 10000, 30000, TimeUnit.MILLISECONDS);
+			;
 		}
 	}
 
