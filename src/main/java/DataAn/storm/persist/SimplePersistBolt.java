dfd8cb9e1d46a4b0f8785fa5b290a04594f5665a
jiazhongjin <jiazj@fengyuntec.com>
2016-11-25 14:00:30 +0800

    u

--- src/main/java/DataAn/storm/persist/SimplePersistBolt.java
+++ src/main/java/DataAn/storm/persist/SimplePersistBolt.java
@@ -1,26 +1,33 @@
 package DataAn.storm.persist;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
+import DataAn.storm.Communication;
 import DataAn.storm.hierarchy.BaseSimpleRichBolt;
-import DataAn.storm.kafka.Notify;
 
 @SuppressWarnings({"serial","unused"})
 public class SimplePersistBolt extends BaseSimpleRichBolt {
-
-	public SimplePersistBolt(Fields fields) {
-		super(fields);
+	
+	public SimplePersistBolt() {
+		super(new Fields("records","communication"));
 	}
 
 	@Override
 	protected void doExecute(Tuple tuple) throws Exception {
 		List<MongoPeristModel> mongoPeristModels= 
 				(List<MongoPeristModel>) tuple.getValueByField("records");
+		Communication communication= 
+				(Communication) tuple.getValueByField("communication");
 		IMongoPersistService mongoPersistService=  IMongoPersistService.MongoPersistServiceGetter.getMongoPersistService(getStormConf());
 		mongoPersistService.persist(mongoPeristModels, getStormConf());
+		
+		emit(new Values(new Date().getTime(),communication));
+		
 //		try{
 //			for(MongoPeristModel mongoPeristModel:mongoPeristModels){
 //				mongoPersistService.persist(mongoPeristModel, getStormConf());
