cb94ebe57c7295c14d5dd62dc98a7b9a6fe1a04e
Shenwp <Shenwp@Shenwp-PC>
2016-11-25 10:20:53 +0800

    shenwp

--- src/test/java/DataAn/storm/persist/MongoPersistServiceTest.java
+++ src/test/java/DataAn/storm/persist/MongoPersistServiceTest.java
@@ -18,7 +18,6 @@ import DataAn.storm.kafka.Notify;
 public class MongoPersistServiceTest {
 
 	private J9SeriesService j9SeriesService;
-	private String filePath = "E:\\data\\flywheel\\2000\\01\\j9-02--2000-01-01.csv";
 	
 	@Before
 	public void init(){
@@ -26,14 +25,47 @@ public class MongoPersistServiceTest {
 	}
 	
 	@Test
-	public void test() throws Exception{
+	public void testInsertOne() throws Exception{
+		List<MongoPeristModel> mongoPeristModels= this.getMongoPeristModelList();
+		System.out.println("mongoPeristModels size: " + mongoPeristModels.size());
+		IMongoPersistService mongoPersistService=  IMongoPersistService.MongoPersistServiceGetter.getMongoPersistService(null);
+		try{
+			long begin = System.currentTimeMillis();
+			System.out.println("begin insert one...");
+			for(MongoPeristModel model:mongoPeristModels){
+				mongoPersistService.persist(model, null);
+			}
+			long end = System.currentTimeMillis();
+			System.out.println("end insert one time: " + (end - begin) + " mm");
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	@Test
+	public void testInsertMany() throws Exception{
+		List<MongoPeristModel> mongoPeristModels= this.getMongoPeristModelList();
+		System.out.println("mongoPeristModels size: " + mongoPeristModels.size());
+		IMongoPersistService mongoPersistService=  IMongoPersistService.MongoPersistServiceGetter.getMongoPersistService(null);
+		try{
+			long begin = System.currentTimeMillis();
+			System.out.println("begin insert many...");
+			mongoPersistService.persist(mongoPeristModels, null);
+			long end = System.currentTimeMillis();
+			System.out.println("end insert many time: " + (end - begin) +  " mm");
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+	private List<MongoPeristModel> getMongoPeristModelList() throws Exception{
+		String filePath = "E:\\data\\flywheel\\2000\\05\\j9-02--2000-05-01.csv";
 		String series = SeriesType.J9_SERIES.getName();
 		String star = J9SeriesType.STRA2.getValue();
 		String[] collections = {"flywheel1s", "flywheel5s", "flywheel30s", "flywheel1m"};
 		String versions = UUIDGeneratorUtil.getUUID();
 		System.out.println("versions: " + versions);
 		List<Document> docList = j9SeriesService.readCSVFileToDoc(filePath, versions);
-		System.out.println("docList size: " + docList.size());
+		System.out.println("数据总数: " + docList.size());
 		List<MongoPeristModel> mongoPeristModels= new ArrayList<MongoPeristModel>();
 		MongoPeristModel mongoPeristModel = null;
 		for (Document doc : docList) {
@@ -44,15 +76,6 @@ public class MongoPersistServiceTest {
 			mongoPeristModel.setContent(doc.toJson());
 			mongoPeristModels.add(mongoPeristModel);
 		}
-		System.out.println("mongoPeristModels size: " + mongoPeristModels.size());
-		IMongoPersistService mongoPersistService=  IMongoPersistService.MongoPersistServiceGetter.getMongoPersistService(null);
-		try{
-			for(MongoPeristModel model:mongoPeristModels){
-				mongoPersistService.persist(model, null);
-			}
-		}catch (Exception e) {
-			throw e;
-		}
+		return mongoPeristModels;
 	}
-	
 }
