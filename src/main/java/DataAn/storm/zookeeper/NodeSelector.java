16871ded68c78aab5fe9014c78aadb5a15cc2f5b
jiazhongjin <jiazj@fengyuntec.com>
2016-12-26 15:40:34 +0800

    u

--- src/main/java/DataAn/storm/zookeeper/NodeSelector.java
+++ src/main/java/DataAn/storm/zookeeper/NodeSelector.java
@@ -701,7 +701,7 @@ public class NodeSelector implements Serializable{
 	}
 	
 	private void attachWorkersPathWatcher(final Workflow workflow){
-		String _path=workflow.pluginWorkersPath;
+		String _path=reportWorkersPath();
 		final PathChildrenCache cache= executor.watchChildrenPath(_path, 
 				new ZooKeeperClient.NodeChildrenCallback() {
 			@Override
