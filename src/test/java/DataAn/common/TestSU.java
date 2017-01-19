23ef2351b2291002c725f54272219e6e716bb468
Shenwp <Shenwp@Shenwp-PC>
2017-01-19 10:14:26 +0800

    shenwp

--- src/test/java/DataAn/common/TestSU.java
+++ src/test/java/DataAn/common/TestSU.java
@@ -5,33 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Test;
+
 
 
 public class TestSU {
 	
-	public static void main(String args[]){
-		Object[] joblistCatch = new Object[10];
-		
-		for(int i=0;i<5;i++){
-			List<String>  csDtoCatch = new ArrayList<>();
-			joblistCatch[i] = csDtoCatch;
-		}
-//		Map<String,String> aa =new HashMap<>();
-//		aa.put("x", "a");
-//		aa.put("x", "b");
-		
-		for(int i=0;i<5;i++){
-			List<String>  cc = (List<String>) joblistCatch[i];
-			cc.add(i+"aa");
-		}
-		
-		for(int i=0;i<5;i++){
-			List<String>  cc = (List<String>) joblistCatch[i];
-			
-			System.out.println(cc.get(0));
-		}
-		
-		
-		
+	@Test
+	public void test(){
+		System.out.println("flywheel_job".endsWith("job"));
 	}
 }
