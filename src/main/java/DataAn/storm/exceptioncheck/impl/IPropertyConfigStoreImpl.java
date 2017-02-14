e0f44a9326ed49b31fb4ec1a995c805a5ceb75c6
TRhanz <hanz@tongyuan.cc>
2017-02-14 16:01:13 +0800

    hanz

--- src/main/java/DataAn/storm/exceptioncheck/impl/IPropertyConfigStoreImpl.java
+++ src/main/java/DataAn/storm/exceptioncheck/impl/IPropertyConfigStoreImpl.java
@@ -130,6 +130,7 @@ public class IPropertyConfigStoreImpl implements IPropertyConfigStore{
 		 //String entity = HttpUtil.get(serverConfig+"/DataRemote/Communicate/getWarnValueByParam?series="+series+"&star="+star+"&parameterType="+parameterType+"");
 		Map<String,String> paramCode_deviceName_map = new HashMap<String,String>();
 		String entity = HttpUtil.get(serverConfig+"/DataRemote/Communicate/getExceptionJobConfigList?series="+series+"&star="+star+"&parameterType="+parameterType+""); 
+		System.out.println(entity);
 		if(StringUtils.isNotBlank(entity)){
 			Map<String,Object> map = JJSON.get().parse(entity);						
 			//机动规则
@@ -198,6 +199,7 @@ public class IPropertyConfigStoreImpl implements IPropertyConfigStore{
 					expointconf.setParamCode(exceConfig.getParamCode());
 					expointconf.setMax(max);
 					expointconf.setMin(min);
+					System.out.println("设置陀螺名"+exceConfig.getParamCode()+"|"+topName);
 					expointconf.setTopName(topName);
 					toppointconfigmap.put(expointconf.getParamCode(), expointconf);	
 				}
